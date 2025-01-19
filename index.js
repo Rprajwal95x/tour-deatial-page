3f922025058cdc763b9637b5df790c4832bb5c93
Rprajwal95x <rprajwal9529@gmail.com>
2025-01-19 20:34:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('popupOverlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('popupOverlay').style.display = 'none';
+}
+
+function bookNow() {
+    alert('Booking functionality coming soon!');
+}
+  
